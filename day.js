90ac235a46a11c8bd20e55cb250ef11f6630f046
werronense <31091965+werronense@users.noreply.github.com>
2019-06-27 12:13:56 -0400

    Refactor alert

---
+++ day.js
@@ -0,0 +1,8 @@
+function dayName(date) {
+    const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return daysOfTheWeek[date.getDay()];
+}
+
+function greeting(date) {
+  return `Hello, world! Happy ${dayName(date)}!`;
+}
