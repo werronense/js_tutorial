34bf46b4dfececb246eb38a6a0ab76af26048698
werronense <31091965+werronense@users.noreply.github.com>
2019-06-28 09:02:44 -0400

    Update palindrome checker

--- bundle.js
+++ bundle.js
@@ -1,14 +1,28 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let Phrase = require("werronense-palindrome");
-let string = prompt("Please enter a string for palindrome testing:");
 
-let phrase = new Phrase(string);
+// prompt user input, determine whether palindrome or not
+function palindromeTester(event) {
+  event.preventDefault();
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome.`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`);
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome.`;
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+  }
 }
+
+// add event listeners after the DOM loads
+document.addEventListener("DOMContentLoaded", function(){
+  // select the button and add a click event listener
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event){
+    palindromeTester(event);
+  });
+});
 
 },{"werronense-palindrome":2}],2:[function(require,module,exports){
 module.exports = Phrase;
@@ -34,7 +48,11 @@ function Phrase(content) {
 
   // returns true if palindrome, false otherwise
   this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().reverse();
+    if (this.processedContent()) {
+      return this.processedContent() === this.processedContent().reverse();
+    } else {
+      return false;
+    }
   }
 }
 
--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 let Phrase = require("werronense-palindrome");
-let string = prompt("Please enter a string for palindrome testing:");
 
-let phrase = new Phrase(string);
+// prompt user input, determine whether palindrome or not
+function palindromeTester(event) {
+  event.preventDefault();
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome.`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`);
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+    palindromeResult.text = `"${phrase.content}" is a palindrome.`;
+  } else {
+    palindromeResult.text = `"${phrase.content}" is not a palindrome.`;
+  }
 }
+
+// add event listeners after the DOM loads
+document.addEventListener("DOMContentLoaded", function(){
+  // select the button and add a click event listener
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event){
+    palindromeTester(event);
+  });
+});
