7498164d618153f13dcbcbe476a2def772a58170
werronense <31091965+werronense@users.noreply.github.com>
2019-06-28 07:42:58 -0400

    Prevent Jekyll build

---
+++ bundle.js
@@ -0,0 +1,41 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+let Phrase = require("werronense-palindrome");
+let string = prompt("Please enter a string for palindrome testing:");
+
+let phrase = new Phrase(string);
+
+if (phrase.palindrome()) {
+  alert(`"${phrase.content}" is a palindrome.`);
+} else {
+  alert(`"${phrase.content}" is not a palindrome.`);
+}
+
+},{"werronense-palindrome":2}],2:[function(require,module,exports){
+module.exports = Phrase;
+
+// adds reverse to all strings
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+// defines a phrase object
+function Phrase(content) {
+  this.content = content;
+
+  // returns content processed for palindrome testing
+  this.processedContent = function processedContent() {
+    return this.letters().toLowerCase();
+  }
+
+  // returns the letters in the content
+  this.letters = function letters() {
+    return (this.content.match(/[a-zA-Z]/gi) || []).join("");
+  }
+
+  // returns true if palindrome, false otherwise
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
+
+},{}]},{},[1]);
--- day.js
+++
@@ -1,8 +0,0 @@
-function dayName(date) {
-    const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    return daysOfTheWeek[date.getDay()];
-}
-
-function greeting(date) {
-  return `Hello, world! Happy ${dayName(date)}!`;
-}
